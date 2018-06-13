2c9a07982f46e64b5bc70e8d57b15da07c0efdda
abnunez <adam.b.nunez@gmail.com>
2018-06-13 13:08:14 -0500

    add da things

---
+++ Assets/game/core.clj
@@ -0,0 +1,18 @@
+(ns game.core
+  (:use arcadia.core arcadia.linear)
+  (:import [UnityEngine Collider2D Physics
+            GameObject Input Rigidbody2D Rigidbody
+            Vector2 Mathf Resources Transform
+            Collision2D Physics2D]
+           ArcadiaState)
+  (:require [game.movement :as m])
+  )
+
+  (def player-roles
+    {::movement {:fixed-update #'game.movement/player-movement-fixed-update}})
+
+  (defn setup []
+    (let [player  (object-named "unitychan")]
+    ;  (reset! fighter-atom fighter)
+      (roles+ player player-roles)
+      )) ; NEW
